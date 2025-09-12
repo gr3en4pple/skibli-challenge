bc3193aecf7c8bc6f3d3ffd340580131ded014f5
gr3en4pple <gr3en4pple23@gmail.com>
2025-09-12 15:26:41 +0700

    formatString challenge

---
+++ index.js
@@ -0,0 +1,11 @@
+// write a method accepts a string value and the output must not
+// contain any whitespace larger than 1 character length and without
+// bad_words such as 'damn' or 'bullshit'
+
+const formatString = (string) => {
+  if (typeof string !== "string" || !string) return string;
+  const removeWhiteSpace = string.replace(/\s+/g, " ");
+  const badWords = ["damn", "bullshit"];
+  const splittedString = removeWhiteSpace.split(" ");
+  return splittedString.filter(s => !badWords.includes(s)).join(" ")
+};
